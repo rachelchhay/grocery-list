1e430bae487e82179b9b578611bbdca74aa95fd1
Rachel Chhay <rachelchhay@rachels-mbp.attlocal.net>
2019-10-26 15:25:50 -0500

    Item component is set up.

---
+++ grocery-list-app/src/Item.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Item extends Component {
+    render() {
+        return (
+            <div>
+                Item component is working!
+            </div>
+        );
+    }
+}
+
+export default Item;
--- grocery-list-app/src/Items.js
+++ grocery-list-app/src/Items.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Item from './Item';
 
 class Items extends Component {
     render() {
         return (
             <div>
                 Items component working!
+                <Item />
             </div>
         );
     }
