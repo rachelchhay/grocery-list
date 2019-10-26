c1b52caf1f4a27f740abb87be0e39e0478b974e4
Rachel Chhay <rachelchhay@rachels-mbp.attlocal.net>
2019-10-26 15:10:37 -0500

    Components created. AddItems Component is set up.

---
+++ grocery-list-app/src/AddItems.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddItems extends Component {
+    render() {
+        return (
+            <div>
+                Add Items working!
+            </div>
+        );
+    }
+}
+
+export default AddItems;
