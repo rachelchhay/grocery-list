7e32530397f9e379a4e9e3845fa481664f4d1926
Rachel Chhay <rachelchhay@rachels-mbp.attlocal.net>
2019-10-26 16:02:41 -0500

    form added and cta works

--- grocery-list-app/src/AddItems.js
+++ grocery-list-app/src/AddItems.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 import Items from './Items';
 
 class AddItems extends Component {
+    state = {
+        value: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            value: event.target.value
+        });
+    }
+
+    handleSubmit = (event) => {
+        console.log('Value submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div>
-                Add Items working!
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Grocery Item: 
+                        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
                 <Items />
             </div>
         );
